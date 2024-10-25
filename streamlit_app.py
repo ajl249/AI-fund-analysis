837f0ceced847faad192fd43490ee18ff21a41ab
Adam Lawson <ajl249@exeter.ac.uk>
2024-10-25 11:50:33 +0000

    Changes to be committed:     modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 AI fund Stock Analysis")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Input a Ticker to Begin Analysis"
 )
